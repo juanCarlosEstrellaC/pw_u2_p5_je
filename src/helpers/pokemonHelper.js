a46b5c56c93bd47a40b3b1505bc421e25432ba66
Juan Estrella <jcestrella@uce.edu.ec>
2024-01-17 20:41:16 -0500

    taller23, ciclo de vida de componentes

--- src/helpers/pokemonHelper.js
+++ src/helpers/pokemonHelper.js
@@ -5,21 +5,53 @@ const obtenerIdPokemons = () => {
     const pokemon4 = obtenerNumeroAleatorio(1,600);
     const pokemonsArr = [pokemon1, pokemon2, pokemon3, pokemon4]
     console.log(pokemonsArr);
+    return pokemonsArr;
  }
 
-const obtenerNombresPokemons = (arreglo) => {
-    const nombre1 = consumirAPI(arreglo[0]);
-    const nombre2 = consumirAPI(arreglo[1]);
-    const nombre3 = consumirAPI(arreglo[2]);
-    const nombre4 = consumirAPI(arreglo[3]);
+const obtenerNombresPokemons = async ([p1, p2, p3, p4]) => {
+    const nombre1 = await consultarNombre(p1);
+    const nombre2 = await consultarNombre(p2);
+    const nombre3 = await consultarNombre(p3);
+    const nombre4 = await consultarNombre(p4);
+
+    const pk1 = {
+        id:p1,
+        nombre:nombre1
+    }
+    const pk2 = {
+        id:p2,
+        nombre:nombre2
+    }
+    const pk3 = {
+        id:p1,
+        nombre:nombre3
+    }
+    const pk4 = {
+        id:p4,
+        nombre:nombre4
+    }
+
+    const arreglo = [pk1, pk2, pk3, pk4];
+    console.log(arreglo);
+    return arreglo
 }
 
-function consumirAPI(id){
-    return "nombre"
+const obtenerPokemons = async ()=>{
+    const arregloId =  obtenerIdPokemons()
+    return await obtenerNombresPokemons(arregloId)
 }
 
-const obtenerIdPokemonsFachada = () => {
-    obtenerIdPokemons()
+async function consultarNombre(id){
+    const data = await consumirAPI(id);
+    return data.name;
+}
+
+const consumirAPI = async (id) => {
+    return await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(r=>r.json())
+}
+
+const obtenerPokemonsFachada = async () => {
+    return await obtenerPokemons()
 }
 
 function obtenerNumeroAleatorio(inicio, fin){
@@ -28,4 +60,4 @@ function obtenerNumeroAleatorio(inicio, fin){
 
 
 
-export default obtenerIdPokemonsFachada
+export default obtenerPokemonsFachada
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,26 +1,33 @@
 <template>
   <h1>Adivina el Pokemon</h1>
-  <PokemonImagen :pokemonId="6" :showPokemon="true"/>
-  <PokemonOpciones/>
+  <PokemonImagen :pokemonId="6" :showPokemon="true" />
+  <PokemonOpciones />
 </template>
 
 <script>
 import PokemonImagen from "../components/PokemonImagen.vue";
 import PokemonOpciones from "../components/PokemonOpciones.vue";
 
-import obtenerIdPokemonsFachada from "../helpers/pokemonHelper.js";
-obtenerIdPokemonsFachada()
+import obtenerPokemonsFachada from "../helpers/pokemonHelper.js";
 
 export default {
-components:{
+  components: {
     PokemonImagen,
     PokemonOpciones,
-},
-
-
-}
+  },
+  mounted() {   // Ojo, que mounted no se coloca como otra option api, con ":"
+    console.log("Se montó el componente Pokemon Pages");
+    this.cargaInicial(); 
+  },
+  methods: {
+     async cargaInicial(){
+      const arregloPokemons = await obtenerPokemonsFachada();
+      console.log("Desde componente");
+      console.log(arregloPokemons);
+    }
+  },
+};
 </script>
 
 <style>
-
 </style>
